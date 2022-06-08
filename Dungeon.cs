8f9c9fbdfd7a00ffc782a8be87d18a5fd40068b3
heyaaronschmitt <106359099+heyaaronschmitt@users.noreply.github.com>
2022-06-07 20:52:26 -0500

    Add project files.

---
+++ Dungeon.cs
@@ -0,0 +1,64 @@
+﻿namespace Dungeon
+{
+    internal class DungeonApplication
+    {
+        static void Main(string[] args)
+        {
+            //TODO Create A Player
+
+            bool exit = false;
+
+            do
+            {
+                do
+                {
+
+                    //TODO Create A Monster
+
+                    //TODO Create A Room
+
+                    bool reload = false;
+
+                    Console.WriteLine("A. Attach\nB. Run Away\nC. Character Info\nD.Monster Info \nE. Exit");
+                    string userChoice = Console.ReadLine().ToUpper();
+                    Console.Clear();
+
+                    switch (userChoice)
+                    {
+                        case "A":
+                            break;
+
+                        case "B":
+                            break;
+
+                        case "C":
+                            break;
+
+                        case "D":
+                            break;
+
+                        case "E":
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                } while (true);
+            } while (true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ DungeonLibrary/Character.cs
@@ -0,0 +1,48 @@
+﻿namespace DungeonLibrary
+{
+    public class Character
+    {
+        //Funny  - Fields
+        /*
+            name – string
+            hitChance – int
+            block – int
+         */
+        private int _life;
+        private int _maxLife;
+        //People - Properties (props)
+        public int MaxLife
+        {
+            get { return _maxLife; }
+            set { _maxLife = value; }
+        }
+        //Alternative shortcut, only if we don't have business rules
+        //public int MaxLife { get; set; }
+        public int Life
+        {
+            get { return _life; }
+            set
+            {
+                //Business rule: Life should not be MORE than MaxLife
+                if (value <= MaxLife)
+                {
+                    //good to go
+                    _life = value;
+                }//end if
+                else
+                {
+                    _life = MaxLife;
+                }//end else
+            }//end set
+        }//end Life prop
+
+        //Collect - Constructors (ctors)
+        //1 fully qualified constructor
+
+        //Monkeys - Methods
+        //ToString()
+        //CalcBlock() -> return Block
+        //CalcHitChance() -> return HitChance
+        //CalcDamage() -> Return 0;
+    }
+}
